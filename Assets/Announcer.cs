b99efea09908a449a79f49912cea4b15827e765d
Ryan Zhang <zhangryan09@gmail.com>
2025-06-12 13:38:27 -0400

    Added tts introduction framework

--- Assets/Announcer.cs
+++ Assets/Announcer.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Meta.WitAi.TTS.Utilities;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Announcer : MonoBehaviour
 {
     public static Announcer instance;
     
     [SerializeField] private Transform follow;
+    [SerializeField] private AudioMixer audioMixer;
 
     private TTSSpeaker _speaker;
     
@@ -27,7 +29,23 @@ public class Announcer : MonoBehaviour
     void Start()
     {
         _speaker = GetComponent<TTSSpeaker>();
+        audioMixer.SetFloat("Volume (of Sound Effects)", -80f);
+        audioMixer.SetFloat("Volume (of Sound Effects 3)", -80f);
+        // TODO: turn into coroutine
         _speaker.Speak("Welcome to the VR Cane Simulation!");
+        _speaker.SpeakQueued("Your goal is to reach the bus, indicated by this sound: ");
+        audioMixer.SetFloat("Volume (of Sound Effects 2)", 0.0f);
+        // Event to start playing bus sound.
+        // wait for sound to finish playing
+        _speaker.SpeakQueued("When your cane touches the ground it will vibrate and make a sound like this: ");
+        // Event to start playing ground scrape sound.
+        // wait for sound to finish playing
+        audioMixer.SetFloat("Volume (of Sound Effects 2)", -80f);
+        _speaker.SpeakQueued("When you touch an obstacle, a different sound will start playing. This means you should try and avoid it.");
+        _speaker.SpeakQueued("With that, you are now ready to play! Good luck and have fun.");
+        // Unmute all sounds.
+        audioMixer.SetFloat("Volume (of Sound Effects)", 0f);
+        audioMixer.SetFloat("Volume (of Sound Effects 3)", 0f);
     }
 
     // Update is called once per frame
